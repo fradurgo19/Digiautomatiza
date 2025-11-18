ea0b0df5bda4b0f1653c3dc7a37fb24b638f72fd
fradurgo19 <fradurgo19@gmail.com>
2025-11-17 20:39:12 -0500

    fix: agregar headers x-usuario-id y x-usuario-rol a CORS

--- api/login.js
+++ api/login.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,POST');
   res.setHeader(
     'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, x-usuario-id, x-usuario-rol'
   );
 
   if (req.method === 'OPTIONS') {
